adc2875af623b99692679b2b8045adcaf6f1475b
kitakou0313 <kitakou0313@gmail.com>
2024-08-29 08:49:37 +0900

    docs: add memo about function

--- ts-tutorial/section4/function.ts
+++ ts-tutorial/section4/function.ts
@@ -28,3 +28,75 @@ function multiply(a:number,b:number, c?:number) {
     }
     return a * b * c
 }
+
+// Default parameter
+function applyDiscount(price:number, discount:number = 0.05) {
+    return price * (1 - discount)
+}
+applyDiscount(100)
+applyDiscount(100, 0.1)
+
+// 型の定義では使えない
+let promotion: (price: number, discount: number = 0.05) => number
+
+// Rest parameter
+// 数が確定しない引数を扱う
+function getTotalWithRestParam(...numbers:number[]): number {
+    let total = 0
+    numbers.forEach((num) => {
+        total += num
+    })
+    return total
+}
+getTotalWithRestParam()
+getTotalWithRestParam(1)
+getTotalWithRestParam(1,2,3,4)
+
+function combine(...args:(number | string)[]): [number, string] {
+    let total = 0
+    let str = ""
+
+    args.forEach((arg) => {
+        if (typeof arg === "number") {
+            total += arg
+        } else {
+            str += arg
+        }
+    })
+
+    return [total, str]
+}
+
+// FunctionのOverload
+// JavaなどのOverload（型が異なる複数の関数を定義する）とは別 
+
+function addWithOverload(a: number, b:number): number;
+function addWithOverload(a: string, b:string): string;
+
+function addWithOverload(a: any, b:any):any {
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b
+    }else if (typeof a === "string" && typeof b === "string"){
+        return a + b
+    }else{
+        throw new Error("Invalid arguemnts")
+    }
+}
+
+// Any型で定義した引数，返り値の型を事前に定義したSignatureで狭めるイメージ
+addWithOverload(10, 20)
+addWithOverload("aa", "bb")
+// 処理の実装時に引数の型が未確定になることを考慮する必要のないJavaのOverloadとは確かに異なる
+
+// 受け取る引数の数が異なるOverloadの場合
+// 実装時に必須となる引数の数はそろえる必要があるので超過した分はOptionalにする必要がある
+function sumWithOverload(a: number, b: number): number;
+function sumWithOverload(a: number, b: number, c:number): number;
+function sumWithOverload(a: number, b: number, c?:number): number{
+    if(c) {
+        return a + b+ c
+    }
+    return a + b
+};
+
+// Classのmethod（Classのpropertyとして定義された関数）でもOverload可能
