11a5085aa62073598cfdee88a05cda009a0ec9d8
kitakou0313 <kitakou0313@gmail.com>
2024-09-23 12:18:00 +0900

    add:add memo about function compatibility

--- handbook/type-manipulation/type_compatibility.ts
+++ handbook/type-manipulation/type_compatibility.ts
@@ -33,3 +33,31 @@ greet(pet)
 // object型の値では既知のpropertyしか指定できない点に注意
 // Pet interface型を明示的に指定した場合は owner propetyが指定できない
 let dogError:Pet = {name:"Lassie", owner: "Rudd"}
+
+// 関数の比較
+let x_function = (a: number) => 0;
+let y_function = (b: number, s: string) => 0
+
+y_function = x_function;
+x_function = y_function;
+
+// 関数の比較
+// 1. 代入する値（x_function）の全ての引数の型が代入される変数の型（y_function）の引数の型と互換性があるか検証
+//   引数名は不一致で問題ない
+//   上の例ではaがbに互換性があるので代入可能
+//   下の例はsに互換性のある引数がx_functionになかったため互換性が無い
+
+y_function(111, "hoge")
+// 第二引数が無視されるが，これはJavaScriptでは追加の引数を無視する実装が一般的なため
+
+// 2. 返り値は代入される変数の型のpropertyをすべて持つか（= subtypeか）で判定されている
+
+let x_function_return = () => ({name: "Alice"})
+let y_function_return = () => ({name: "Alice", location: "Seatle"})
+
+x_function_return = y_function_return
+y_function_return = x_function_return
+
+// 共通する考え方として，代入される変数の型に期待されるinterfaceと返り値を満たせる場合に互換性があると判定されていそう
+//   引数の場合...代入される変数の型の引数で呼び出されてもエラーが起きない関数であればよい -> 必要な引数が渡されば引数が捨てられても良い
+//   返り値の場合...代入される変数の型の後続処理で参照されるよう，propretyが含まれればよい -> 必要なpropetyを持つなら多くても問題ない
